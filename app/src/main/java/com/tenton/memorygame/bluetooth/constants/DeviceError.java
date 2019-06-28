4b8b3992c16ef1f8c27fb072e4219b2382701515
Zgjim Krasniqi <zgjimkrasniqi27@gmail.com>
2019-06-28 13:41:51 +0200

    Bluetooth changes

--- app/src/main/java/com/tenton/memorygame/bluetooth/constants/DeviceError.java
+++
@@ -1,6 +0,0 @@
-package com.tenton.memorygame.bluetooth.constants;
-
-public class DeviceError {
-    public final static int FAILED_WHILE_CLOSING = 121;
-    public final static int FAILED_WHILE_CREATING_SOCKET = 122;
-}
--- app/src/main/java/com/tenton/memorygame/bluetooth/constants/DiscoveryError.java
+++
@@ -1,7 +0,0 @@
-package com.tenton.memorygame.bluetooth.constants;
-
-public class DiscoveryError {
-    public final static int FAILED_TO_PAIR = 111;
-    public final static int FAILED_TO_UNPAIR = 112;
-    public final static int BLUETOOTH_DISABLED = 113;
-}
